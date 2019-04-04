4cb3410f9687fe0dc0ab5ee14b3269e7578f720a
coding-cc <854327790@qq.com>
2019-04-04 21:13:13 +0800

    first commit

---
+++ src/main/java/com/autobuild/AutobuildApplication.java
@@ -0,0 +1,13 @@
+package com.autobuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutobuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutobuildApplication.class, args);
+    }
+
+}
