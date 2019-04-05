11343a111fc3173b3674e336ca345136efe56e5c
coding-cc <854327790@qq.com>
2019-04-05 15:12:45 +0800

    m

--- src/main/java/com/autobuild/controller/TestController.java
+++ src/main/java/com/autobuild/controller/TestController.java
@@ -1,16 +1,17 @@
 package com.autobuild.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *  * @author cc
  * Created by cc on 2019/04/04
  */
-@Controller
+@RestController
 public class TestController {
     @GetMapping
     public String out(){
         return "hello jenkins";
     }
+
 }
